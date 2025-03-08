6659356c75518b3a69ec070e1fa9503cc01ce09c
inspire12 <ox4443@naver.com>
2025-03-08 12:27:59 +0900

    first commit

---
+++ src/test/java/com/inspire12/likelionsample/LikeLionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspire12.likelionsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
