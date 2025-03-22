9217fdf143c7d98963fea005d902df31cc1ea010
inspire12 <ox4443@naver.com>
2025-03-22 18:05:15 +0900

    feat(testing): jpa 테스트 케이스 실패 처리

--- src/test/java/com/inspire12/likelionbackend/module/order/application/service/OrderServiceTest.java
+++ src/test/java/com/inspire12/likelionbackend/module/order/application/service/OrderServiceTest.java
@@ -6,10 +6,10 @@ import com.inspire12.likelionbackend.module.order.domain.Order;
 import com.inspire12.likelionbackend.module.order.domain.OrderRepository;
 import com.inspire12.likelionbackend.module.order.infrastructure.repository.entity.OrderStatus;
 import com.inspire12.likelionbackend.module.payment.application.service.PaymentProcessOrderUsecase;
-import com.inspire12.likelionbackend.repository.TestOrderRepository;
 import com.inspire12.likelionbackend.testure.OrderMockScenario;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,13 +17,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class OrderServiceTest {
 
-    private OrderService OrderService;
-
+    private OrderService orderService;
+    @Autowired
+    private OrderRepository orderRepository;
     @BeforeEach
     void setup() {
-        OrderRepository orderRepository = new TestOrderRepository();
+//        OrderRepository orderRepository = new TestOrderRepository();
         PaymentPort paymentPort = new PaymentProcessOrderUsecase();
-        OrderService = new OrderService(orderRepository, paymentPort);
+        orderService = new OrderService(orderRepository, paymentPort);
     }
 
     @Test
@@ -31,7 +32,19 @@ class OrderServiceTest {
         // Given
         OrderRequest orderRequest = OrderMockScenario.createDeliveryOrderRequest();
         // When
-        Order order = OrderService.confirmOrderPaymentCase1(orderRequest);
+        // 테스트가 실패, 왜?
+        Order order = orderService.confirmOrderPaymentCase1(orderRequest);
+        // Then
+        assertEquals(OrderStatus.SUCCESS_PAYMENT, order.getOrderStatus());
+    }
+
+    @Test
+    void 결제가_성공한_후_주문상태_확인_mock() {
+        // Given
+        OrderRequest orderRequest = OrderMockScenario.createDeliveryOrderRequest();
+        // When
+        // 테스트가 실패, 왜?
+        Order order = orderService.confirmOrderPaymentCase1(orderRequest);
         // Then
         assertEquals(OrderStatus.SUCCESS_PAYMENT, order.getOrderStatus());
     }
@@ -41,7 +54,7 @@ class OrderServiceTest {
         // Given
         OrderRequest orderRequest = OrderMockScenario.createDeliveryOrderRequest();
         // When
-        Order order = OrderService.confirmOrderPaymentCase1(orderRequest);
+        Order order = orderService.confirmOrderPaymentCase1(orderRequest);
         // Then
         assertEquals(OrderStatus.FAIL_PAYMENT, order.getOrderStatus());
     }
