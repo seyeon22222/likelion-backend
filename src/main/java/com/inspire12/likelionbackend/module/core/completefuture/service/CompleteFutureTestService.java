ede59a0bfb2bf35e0b620eab056eab7445684616
inspire12 <ox4443@naver.com>
2025-03-30 01:13:07 +0900

    feat(core-completefuture-practice): practice

--- src/main/java/com/inspire12/likelionbackend/module/core/completefuture/service/CompleteFutureTestService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/completefuture/service/CompleteFutureTestService.java
@@ -6,28 +6,48 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Service
 public class CompleteFutureTestService {
     private final Executor customFutureThreadPoolExecutor;
-    private Logger log = LoggerFactory.getLogger(CompleteFutureTestService.class);
+    private static Logger log = LoggerFactory.getLogger(CompleteFutureTestService.class);
 
     public CompleteFutureTestService(Executor customFutureThreadPoolExecutor) {
         this.customFutureThreadPoolExecutor = customFutureThreadPoolExecutor;
     }
 
     public void sayHello(int i) {
-        CompletableFuture.supplyAsync(() -> {
-                    log.info("executing Thread Name .. [{}]", Thread.currentThread().getName());
-                    try {
-                        Thread.sleep(1000); // 1초간 정지
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    return "Hello " + i;
-                })
-                .thenApply(s -> s.concat("영학"))
-                .thenAccept(log::info);
+        Supplier<String> heavyTask = new Supplier<String>() {
+            @Override
+            public String get() {
+                log.info("executing Thread Name .. [{}]", Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000); // 1초간 정지
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return "Hello " + i;
+            }
+        };
+        Function<String, String> plusTask = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.concat("영학");
+            }
+        };
+
+        Consumer<String> consume = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                log.info(s);
+            }
+        };
+        CompletableFuture.supplyAsync(/* TODO */)
+                .thenApply(/* TODO */)
+                .thenAccept();
     }
 
     public String getHelloSync() {
