0c5a440b2f9d4f561d773e64ff143c4cf09c958a
inspire12 <ox4443@naver.com>
2025-03-30 02:50:05 +0900

    feat(core-web): request 샘플 추가

--- src/main/java/com/inspire12/likelionbackend/module/mvc/controller/DataController.java
+++ src/main/java/com/inspire12/likelionbackend/module/mvc/controller/DataController.java
@@ -3,6 +3,7 @@ package com.inspire12.likelionbackend.module.mvc.controller;
 import com.inspire12.likelionbackend.module.mvc.model.Customer;
 import com.inspire12.likelionbackend.module.mvc.model.OrderRequest;
 import com.inspire12.likelionbackend.module.mvc.model.OrderResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -23,8 +24,16 @@ public class DataController { // 뜬금 질문, 제가 왜 클래스명을 RestC
                                                  @RequestBody OrderRequest orderRequest,
                                                  @RequestHeader HttpHeaders headers,
                                                  @RequestHeader("user-id") Long userId,
+                                                  HttpServletRequest request,
                                                   Customer customer
                                   ) {
+        String method = request.getMethod(); // 요청 메서드
+        String uri = request.getRequestURI(); // 요청 경로
+        String query = request.getQueryString(); // 쿼리 파라미터
+        String header = request.getHeader("User-Agent"); // 특정 헤더 값
+        String clientIp = request.getRemoteAddr();
+        log.info("메서드: {}\n경로: {}\n쿼리: {}\nUser-Agent 헤더: {}\n ClientIp: {}",
+                method, uri, query, header, clientIp);
 
         log.info("param {}", id);
         log.info("pathVariable {}", username);
