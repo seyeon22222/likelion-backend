525a7bd8cf2df942e093028eb72641ca7339261d
inspire12 <ox4443@naver.com>
2025-03-30 01:55:06 +0900

    feat(core-ioc): 이름 정정

--- src/main/java/com/inspire12/likelionbackend/module/core/ioc/service/BeanShowService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/ioc/service/BeanShowService.java
@@ -1,6 +1,6 @@
 package com.inspire12.likelionbackend.module.core.ioc.service;
 
-import com.inspire12.likelionbackend.module.core.ioc.config.BeanComponentSearchConfig;
+import com.inspire12.likelionbackend.module.core.ioc.config.BeanComponentScanConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -16,7 +16,7 @@ public class BeanShowService {
 
         // ApplicationContext (스프링 컨테이너) 생성
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(BeanComponentSearchConfig.class);
+                new AnnotationConfigApplicationContext(BeanComponentScanConfig.class);
 
         CustomAnnotationService myService = context.getBean(CustomAnnotationService.class);
         myService.sayName();
