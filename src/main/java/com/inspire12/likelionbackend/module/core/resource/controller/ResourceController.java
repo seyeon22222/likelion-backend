fd05e5190795604b9321f3c5051400ff69672ddf
inspire12 <ox4443@naver.com>
2025-03-29 20:49:19 +0900

    feat(core-resource-practice): practice

--- src/main/java/com/inspire12/likelionbackend/module/core/resource/controller/ResourceController.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/resource/controller/ResourceController.java
@@ -13,15 +13,9 @@ import java.io.IOException;
 @RestController
 public class ResourceController {
 
-    private final ResourceService resourceService;
-
-    public ResourceController(ResourceService resourceService) {
-        this.resourceService = resourceService;
-    }
 
     @GetMapping("/load")
     public String load(String resourceName) throws IOException {
-        return resourceService.load(resourceName);
     }
 
     @PostMapping("/write")
@@ -31,10 +25,6 @@ public class ResourceController {
 
     @GetMapping("/load/image")
     public ResponseEntity<byte[]> loadImage() throws IOException {
-        byte[] imageBytes = resourceService.loadImage("oh.jpg");
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.IMAGE_PNG)
-                .body(imageBytes);
     }
 }
--- src/main/java/com/inspire12/likelionbackend/module/core/resource/service/ResourceService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/resource/service/ResourceService.java
@@ -24,25 +24,11 @@ public class ResourceService {
     }
 
     public String load(String resourceName) throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:file/" + resourceName);
-
-        if (resource.exists()) {
-            InputStream inputStream = resource.getInputStream();
-            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-        }
-        throw new FileNotFoundException(resourceName);
+        // TODO /resource/file/likelion.txt 파일 문구를 리턴
     }
 
-    public byte[] loadImage(String resourceName) throws FileNotFoundException {
-        Resource resource = resourceLoader.getResource("classpath:file/" + resourceName);
-
-        byte[] imageBytes = null;
-        try {
-            imageBytes = StreamUtils.copyToByteArray(resource.getInputStream());
-            return imageBytes;
-        } catch (IOException e) {
-            throw new FileNotFoundException(resourceName);
-        }
+    public byte[] loadImage() throws FileNotFoundException {
+        // TODO load를 구현하신 분은 추가로 /resource/file/oh.jpg 이미지 파일 리턴을 해보세요
     }
 
 }
