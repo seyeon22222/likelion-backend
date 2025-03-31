aa1cfcd4259ddde4ef0512a78594e8f2c3852b3a
inspire12 <ox4443@naver.com>
2025-04-01 08:52:39 +0900

    feat(jpa-pager-practice):예제 하나 추가

--- src/main/java/com/inspire12/likelionbackend/module/jpa/repository/OrderJpaRepository.java
+++ src/main/java/com/inspire12/likelionbackend/module/jpa/repository/OrderJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
 
     @Query("select o from OrderEntity o where o.id=:orderId")
     Optional<OrderEntity> findByOrderId(Long orderId);
+
+    List<OrderEntity> findByTotalAmountBetween(Integer totalAmountAfter, Integer totalAmountBefore);
 }
