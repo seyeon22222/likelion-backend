5ce24f037f2b5a9a5c01734e42650e7a13a5ba3a
seykim <kksy0917@gmail.com>
2025-04-01 16:43:22 +0900

    feat(feature/jpa-jpql-practice) : JPQL 실습 완료

--- src/main/java/com/inspire12/likelionbackend/module/jpa/repository/OrderJpaRepository.java
+++ src/main/java/com/inspire12/likelionbackend/module/jpa/repository/OrderJpaRepository.java
@@ -1,8 +1,11 @@
 package com.inspire12.likelionbackend.module.jpa.repository;
 
+import com.inspire12.likelionbackend.module.jpa.model.dto.OrderSum;
 import com.inspire12.likelionbackend.module.jpa.model.dto.OrderSummary;
 import com.inspire12.likelionbackend.module.jpa.model.entity.OrderEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,14 +16,25 @@ import java.util.Optional;
 @Repository
 public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
 
-    @Query("select o from OrderEntity o where o.id=:orderId")
-    Optional<OrderEntity> findByOrderId(Long orderId);
+	@Query("select o from OrderEntity o where o.id=:orderId")
+	Optional<OrderEntity> findByOrderId(Long orderId);
 
-    @Query("SELECT new com.inspire12.likelionbackend.module.jpa.model.dto.OrderSummary(o.id, o.orderNumber, o.totalAmount) " +
-            "FROM OrderEntity o WHERE o.customerId = :customerId")
-    List<OrderSummary> findOrderSummariesByCustomerId(@Param("customerId") Long customerId);
+	@Query(
+		"SELECT new com.inspire12.likelionbackend.module.jpa.model.dto.OrderSummary(o.id, o.orderNumber, o.totalAmount) "
+			+ "FROM OrderEntity o WHERE o.customerId = :customerId")
+	List<OrderSummary> findOrderSummariesByCustomerId(@Param("customerId") Long customerId);
 
-    // TODO
-    @Query("select sum(o.totalAmount) from OrderEntity o where o.customerId=:customerId")
-    Long sumAmountByUserId(Long customerId);
+	// TODO
+	@Query("SELECT new com.inspire12.likelionbackend.module.jpa.model.dto.OrderSum(o.customerId, sum(o.totalAmount)) "
+		+ "FROM OrderEntity o WHERE o.customerId=:customerId")
+	OrderSum sumAmountByUserId(@Param("customerId")Long customerId);
+
+
+	@Query("select o from OrderEntity o where o.id=:Id")
+	Optional<OrderEntity> selectById(Long Id);
+
+	@Modifying
+	@Query("update OrderEntity o set o.totalAmount = :newAmount where o.customerId = :id")
+	void updateAmountById(@Param("id") Long id, @Param("newAmount") Integer newAmount);
+
 }
--- src/main/java/com/inspire12/likelionbackend/module/jpa/service/OrderService.java
+++ src/main/java/com/inspire12/likelionbackend/module/jpa/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.inspire12.likelionbackend.module.jpa.service;
 
+import com.inspire12.likelionbackend.module.jpa.model.dto.OrderSum;
 import com.inspire12.likelionbackend.module.jpa.model.entity.OrderEntity;
 import com.inspire12.likelionbackend.module.jpa.model.mapper.OrderMapper;
 import com.inspire12.likelionbackend.module.jpa.model.request.OrderRequest;
@@ -7,8 +8,10 @@ import com.inspire12.likelionbackend.module.jpa.model.response.OrderResponse;
 import com.inspire12.likelionbackend.module.jpa.model.response.OrderSumResponse;
 import com.inspire12.likelionbackend.module.jpa.model.response.OrderSummaryResponse;
 import com.inspire12.likelionbackend.module.jpa.repository.OrderJpaRepository;
+
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,47 +19,49 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class OrderService {
 
-    private final OrderJpaRepository orderJpaRepository;
+	private final OrderJpaRepository orderJpaRepository;
 
+	@Transactional(readOnly = true)
+	public OrderResponse getOrder(Long orderId) {
+		OrderEntity order = orderJpaRepository.findById(orderId)
+			.orElseThrow(() -> new EntityNotFoundException("주문 없음"));
+		return OrderMapper.fromEntity(order);
+	}
 
-    @Transactional(readOnly = true)
-    public OrderResponse getOrder(Long orderId) {
-        OrderEntity order = orderJpaRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
-        return OrderMapper.fromEntity(order);
-    }
+	@Transactional
+	public OrderResponse saveOrder(OrderRequest request) {
+		OrderEntity order = OrderMapper.toEntity(request);
+		OrderEntity savedOrder = orderJpaRepository.save(order);
+		return OrderMapper.fromEntity(savedOrder);
+	}
 
-    @Transactional
-    public OrderResponse saveOrder(OrderRequest request) {
-        OrderEntity order = OrderMapper.toEntity(request);
-        OrderEntity savedOrder = orderJpaRepository.save(order);
-        return OrderMapper.fromEntity(savedOrder);
-    }
+	@Transactional
+	public void deleteOrder(Long orderId) {
+		OrderEntity order = orderJpaRepository.findById(orderId)
+			.orElseThrow(() -> new EntityNotFoundException("주문 없음"));
+		orderJpaRepository.delete(order);
+	}
 
-    @Transactional
-    public void deleteOrder(Long orderId) {
-        OrderEntity order = orderJpaRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
-        orderJpaRepository.delete(order);
-    }
+	@Transactional
+	public OrderResponse updateTotalAmount(Long orderId, Integer newAmount) {
+		OrderEntity order = orderJpaRepository.findById(orderId)
+			.orElseThrow(() -> new EntityNotFoundException("주문 없음"));
+		order.changeTotalAmount(newAmount);
+		return OrderMapper.fromEntity(order);
+	}
 
-    @Transactional
-    public OrderResponse updateTotalAmount(Long orderId, Integer newAmount) {
-        OrderEntity order = orderJpaRepository.findById(orderId)
-                .orElseThrow(() -> new EntityNotFoundException("주문 없음"));
-        order.changeTotalAmount(newAmount);
-        return OrderMapper.fromEntity(order);
-    }
+	// 고객 ID로 주문 요약 정보 조회
+	@Transactional(readOnly = true)
+	public OrderSummaryResponse getOrderSummaries(Long customerId) {
+		return new OrderSummaryResponse(orderJpaRepository.findOrderSummariesByCustomerId(customerId));
+	}
 
-
-    // 고객 ID로 주문 요약 정보 조회
-    @Transactional(readOnly = true)
-    public OrderSummaryResponse getOrderSummaries(Long customerId) {
-        return new OrderSummaryResponse(orderJpaRepository.findOrderSummariesByCustomerId(customerId));
-    }
-
-    public OrderSumResponse getOrderSum(Long customerId) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
+	public OrderSumResponse getOrderSum(Long customerId) {
+		// TODO
+		OrderSum orderSum = orderJpaRepository.sumAmountByUserId(customerId);
+		if (orderSum.getCustomerId() == null) {
+			throw new EntityNotFoundException("주문 없음");
+		}
+		return new OrderSumResponse(orderSum.getCustomerId(), orderSum.getCount());
+	}
 }
