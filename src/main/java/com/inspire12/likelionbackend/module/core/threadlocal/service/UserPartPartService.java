be5dc37fb02599de7df3a4d8299bfcb7220b8259
seykim <kksy0917@gmail.com>
2025-04-02 14:42:11 +0900

    feat(feature/aop-threadlocal-practice) : 인터셉터 실습 완료

--- src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/service/UserPartPartService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/service/UserPartPartService.java
@@ -4,12 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.inspire12.likelionbackend.module.core.threadlocal.context.UserContextHolder;
+
 @Service
 public class UserPartPartService {
     private static Logger log = LoggerFactory.getLogger(UserPartPartService.class);
+
     public String useCurrentUsername() {
 //        TODO 함수 시그니처를 수정하지 말고 username 을 찍어보기
-        String username = "FIX ME";
+//         String username = "FIX ME";
+        String username = UserContextHolder.getUser();
         log.info("username : {}", username);
         return username;
     }
