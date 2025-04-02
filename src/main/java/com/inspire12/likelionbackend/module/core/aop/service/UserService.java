ca7c183f011d8a0ff4724462b57b428a1556392e
seykim <kksy0917@gmail.com>
2025-04-02 10:12:34 +0900

    feat(feature/aop-start-practice) : 로직 분리 실습 완료

--- src/main/java/com/inspire12/likelionbackend/module/core/aop/service/UserService.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/aop/service/UserService.java
@@ -12,8 +12,25 @@ public class UserService {
     public void registerUser(String username, String email) {
         // 로깅
         long start = System.currentTimeMillis();
+        logStartExecutionTime(username);
+        validateUserInput(username, email);
+        log.info("DB에 사용자 저장 로직 실행");
+        long end = System.currentTimeMillis();
+        logEndExecutionTime(username, end-start);
+    }
+
+    private void logStartExecutionTime(String username) {
+        // TODO registerUser 에서 로깅을 담당하는 파트를 담는다
         log.info("사용자 등록 시작 - username: {}", username);
-        // registerUser 에서 유저 입력 유효성 체크를 담당하는 파트를 담는다
+    }
+
+    private void logEndExecutionTime(String username, long end) {
+        // TODO registerUser 에서 로깅을 담당하는 파트를 담는다
+        log.info("사용자 등록 완료 - username: {} 걸린 시간: {}", username, end);
+    }
+    private void validateUserInput(String username, String email) {
+        // TODO registerUser 에서 유저 입력 유효성 체크를 담당하는 파트를 담는다
+        // 유효성 검사
         if (username == null || email == null) {
             throw new IllegalArgumentException("입력값이 비어 있습니다.");
         }
@@ -21,26 +38,6 @@ public class UserService {
         if (!email.endsWith("@example.com")) {
             throw new SecurityException("허용되지 않은 이메일 도메인입니다.");
         }
-        // 핵심 로직
-        log.info("DB에 사용자 저장 로직 실행");
-        // 로깅
-        long end = System.currentTimeMillis();
-        log.info("사용자 등록 완료 - username: {} 걸린 시간: {}", username, end - start);
-    }
-
-    private void logStartExecutionTime(String username) {
-        // TODO registerUser 에서 로깅을 담당하는 파트를 담는다
-
-    }
-
-    private void logEndExecutionTime(String username, long end) {
-        // TODO registerUser 에서 로깅을 담당하는 파트를 담는다
-
-    }
-    private void validateUserInput(String username, String email) {
-        // TODO registerUser 에서 유저 입력 유효성 체크를 담당하는 파트를 담는다
-        // 유효성 검사
-
     }
 
 }
