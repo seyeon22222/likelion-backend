c4f35a797b74ed40a028f8cb67fe66f14c026046
inspire12 <ox4443@naver.com>
2025-04-03 00:10:03 +0900

    feat(testing-start-practice): practice

---
+++ src/test/java/com/inspire12/likelionbackend/module/order/domain/OrderTest.java
@@ -0,0 +1,14 @@
+package com.inspire12.likelionbackend.module.order.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderTest {
+
+    @Test
+    void 우선_실패하는테스트_만들기() {
+        // TODO
+    }
+
+}
