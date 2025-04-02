526aee87b10fe107420ca4dc4a039967eca1a280
seykim <kksy0917@gmail.com>
2025-04-02 15:18:39 +0900

    feat(feature/aop-threadlocal-filter-practice) : filter 실습 완료

--- src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/filter/AuthFilter.java
+++ src/main/java/com/inspire12/likelionbackend/module/core/threadlocal/filter/AuthFilter.java
@@ -5,15 +5,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 public class AuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         // TODO
+        String header = request.getHeader("X-USER-NAME");
+        log.info("UserContextHolder before : {}", UserContextHolder.getUser());
+        UserContextHolder.setUser(header);
+        log.info("UserContextHolder after : {}", UserContextHolder.getUser());
         try {
             filterChain.doFilter(request, response);
         } finally {
